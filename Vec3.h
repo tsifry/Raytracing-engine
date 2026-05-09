fe5b1ce278cfc030637604c177e4e7999b4267ba
tsifry <150220c@gmail.com>
2026-05-08 21:00:21 -0300

    Added Vector3 dot product, magnitude and normalize functions

--- Vec3.h
+++ Vec3.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <math.h>
 
 struct Vector3{
   float x, y, z;
@@ -13,4 +14,21 @@ struct Vector3{
     return Vector3(x - v.x, y - v.y, z - v.z);
   }
 
+  float static dotProduct(const Vector3 v1, const Vector3 v2){
+    return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+  }
+
+  float static magnitude(const Vector3 vector){
+    return sqrt(dotProduct(vector, vector));
+  }
+
+  Vector3 static normalized(const Vector3 vector){
+    float mag = magnitude(vector);
+    
+    if (mag > 0.0f) {
+        return Vector3(vector.x / mag, vector.y / mag, vector.z / mag);
+    }
+
+    return Vector3(0, 0, 0);
+  }
 };
